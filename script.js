360eb96a2a33188221a4aff5551ff5f79c597978
mariahelenanoren <marialinder97@hotmail.com>
2020-11-09 19:56:44 +0100

    added conditions for empty inputs and button to remove activities

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ window.onload = startFunctions;
 
 function startFunctions() {
     setEventListeners();
+    openAndCloseActivityInputs();
 }
 
 function setEventListeners() {
     const activityButton = document.querySelector("#new-activity-button"); 
     const submitButton = document.querySelector("#submit");
-    
-    activityButton.addEventListener("click", openActivityInputs);
-    submitButton.addEventListener("click", getInputValues);
+
+    activityButton.addEventListener("click", openAndCloseActivityInputs);
+    submitButton.addEventListener("click", function() {
+        getInputValues();
+        openAndCloseActivityInputs();
+    });
 }
 
-function openActivityInputs() {
+function openAndCloseActivityInputs() {
     const activityInputs = document.querySelector("#inputs");
 
-    activityInputs.style.display = "unset";
+    if (activityInputs.style.display == "none") {
+        activityInputs.style.display = "unset";
+    } else {
+        activityInputs.style.display = "none"
+    }
+
 }
 
 function getInputValues() {
@@ -30,5 +39,33 @@ function getInputValues() {
 }
 
 function presentInputValues(inputList) {
-    console.log(inputList);
+    const newDiv = document.createElement("div");
+    const uniqueId = Date.now();
+    newDiv.setAttribute("id", uniqueId);
+    console.log(inputList.value)
+
+    const inputUl = document.createElement("ul");
+    for (i = 0; i < inputList.length; i++) {
+        if (inputList[i].length >= 1) {
+            console.log("input" + inputList[i])
+            const li = document.createElement("li");
+            li.innerHTML = inputList[i];
+            inputUl.appendChild(li);
+        }
+    }
+    newDiv.appendChild(inputUl)
+    const removeButton = document.createElement("button");
+    removeButton.setAttribute("class", "remove-button")
+    removeButton.innerHTML = "Remove activity"
+
+    newDiv.appendChild(removeButton)
+
+    const inputContainer = document.querySelector("#activity-container");
+    inputContainer.appendChild(newDiv)
+
+    removeButton.addEventListener("click", removeActivity, removeButton)
+}
+
+function removeActivity(button) {
+    button.path[2].removeChild(button.path[1]);
 }
